b211ef5a100db901bd435550ea5ca97dc2f678e2
Purushottam <Pk1998.bca@gmail.com>
2022-09-19 23:13:29 +0530

    Create ExeptionC.java

---
+++ Java How To Program Exercises/src/chapter11/ExeptionC.java
@@ -0,0 +1,12 @@
+// Exercise number 11.16 page 514
+// Exercise from Java:How to program 10th edition
+
+package chapter11;
+
+public class ExeptionC extends ExeptionB {
+
+	@Override
+	public String getMessage() {
+		return String.format("Exeption C");
+	}
+}
